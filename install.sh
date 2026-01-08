eed3b4bf51f54dd668b88336dac3437d4b676894
anonyss404 <anonyss404@gmail.com>
2026-01-08 16:35:22 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,8 +4,7 @@ echo -e "\033[1;34m[*] MENGHUBUNGI SERVER ANONYS404...\033[0m"
 sleep 2
 pkg install wget curl -y
 # DOWNLOAD BINARY (Ganti USERNAME & REPO sesuai akun lo!)
-wget -q https://raw.githubusercontent.com/USERNAME_LO/REPO_LO/main/.data/anonymod -O anonymod
-chmod +x anonymod
+wget https://raw.githubusercontent.com/anonys404/ANONYS-V2/main/.data/anonymod -O anonymod
 clear
 echo -e "\033[1;32m[✓] INSTALLASI SELESAI!\033[0m"
 echo -e "\033[1;33mKetik './anonymod' untuk mulai.\033[0m"
